429535c244da28654be243f135f19da5697d0c6a
Jaycob White A <98535121+jwhite6@users.noreply.github.com>
2022-04-05 03:34:40 +0000

    midterm

--- README.md
+++ README.md
@@ -1,29 +1,29 @@
 #### CS 110
-# Chapter 4 - Lab - Functions
-
-### [Assignment Description](https://docs.google.com/document/d/1V20D_upUX4MO8YmskKlRB25Yu2pCEv3-h8z4EAfrSno/edit?usp=sharing)
+# Midterm - Python Programming
 
 ***
 
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
+What did you clean up?:
 
-In this lab, I learned how to create a graph using turtle and graphed trigonometric equations. I also learned about the use of functions, and how the math library works.
+I got rid of a couple lines of code where I picked the pen up and down for no reason. This helped it look cleaner. I also created better variable names, and made a better use of parameters.
 
-## GRACE DAYS
-Grace days used for this assignment: 0
+Summary of function(s) added:
 
-Grace days remaining: 5
+I first added a drawSecantCurve function. I created this by drawing a curve that is (1/cos). I then added a drawBigShape function where I ask the user for the number of sides, the size of each side, and the color they want their shape. After getting this information, the turtle draws the big shape and calculates its perimeter. I then added another function called drawLittleShape, which has constraints, so the shape has to be smaller than the big shape. It goes through the same process as the big shape function.  
+
+Summary of Feature Added:
+
+I ask the user for the number of sides of the shape, length of each side of the shape, and color that they want it. This is the case for both the big shape and little shape. I also calculated the perimeter of each, and at the end of the program, I divide the big shapes perimeter by the little shapes perimeter to figure out the factor of how much greater the perimeter of the big shape is compared to the little one.
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
-
-None
+ 
 
 ## REFERENCES:
 
-https://docs.python.org/3.8/library/turtle.html#turtle.setworldcoordinates
+https://docs.python.org/3/library/turtle.html
 
 ## MISCELLANEOUS COMMENTS:
-
-None
+ 
--- main.py
+++ main.py
@@ -11,30 +11,38 @@ def drawSineCurve(dart):
   dart.up()
   dart.goto(-360, 0)
   dart.down()
-  for i in range(-360, 361):
-    y = math.sin(math.radians(i)) 
+  for i in range(-360, 360):
+    y = math.sin(math.radians(i))
     dart.goto(i, y)
-
+  
 def setupWindow(wn = None):
   wn.bgcolor("lightgreen")
-  wn.setworldcoordinates(-360, -1, 361, 1)
+  wn.setworldcoordinates(-360, -3, 361, 3)
   
 def setupAxis(dart = None):
   dart.goto(-360, 0)
-  dart.goto(360, 0)
+  dart.goto(361, 0)
   dart.up()
-  dart.goto(0, 1)
+  dart.goto(0, 3)
   dart.down()
-  dart.goto(0, -1)
+  dart.goto(0, -3)
+
+def setupAxisForShape(dart = None):
+  dart.goto(-360, 0)
+  dart.goto(361, 0)
+  dart.up()
+  dart.goto(0, 361)
+  dart.down()
+  dart.goto(0, -360)
   
 def drawCosineCurve(dart = None):
   dart.up()
-  dart.goto(-360, 0)
+  dart.goto(-360, 1)
   dart.down()
   for i in range(-360, 361):
     y = math.cos(math.radians(i)) 
-    dart.goto(i, y)
-    
+    dart.goto(i, y) 
+  
 def drawTangentCurve(dart = None):
   dart.up()
   dart.goto(-360, 0)
@@ -42,9 +50,48 @@ def drawTangentCurve(dart = None):
   for i in range(-360, 361):
     y = math.tan(math.radians(i)) 
     dart.goto(i, y)
+
+def drawSecantCurve(dart = None):
+  dart.up()
+  dart.goto(-360, 1)
+  dart.down()
+  for i in range(-360, 361):
+    y = math.cos(math.radians(i))
+    sec = (1 / y)
+    dart.goto(i, sec)
+
+def drawBigShape(dart = None, wn = None, sides = 0, length = 0, color = None, perimeterB = 0):
+  dart.clear()
+  wn.setworldcoordinates(-360, -360, 361, 361)
+  setupAxisForShape(dart)
+  dart.up()
+  dart.goto(0, 0)
+  dart.color(color)
+  dart.down()
+  for i in range(sides):
+    dart.stamp()
+    dart.forward(length)
+    dart.left(360 / sides)
+  dart.color("black")
+  perimeterB = sides * length
+  return perimeterB
+  
+def drawLittleShape(dart = None, wn = None, sides = 0, length = 0, color = None, perimeterL = 0):
+  wn.setworldcoordinates(-360, -360, 361, 361)
+  setupAxisForShape(dart)
+  dart.up()
+  dart.goto(100, 100)
+  dart.color(color)
+  dart.down()
+  for i in range(sides):
+    dart.stamp()
+    dart.forward(length)
+    dart.left(360 / sides)
+  perimeterL = sides * length
+  return perimeterL
   
 ##########  Do Not Alter Any Code Past Here ########
-    
+  
 def main():
     #Part A
     wn = turtle.Screen()
@@ -60,6 +107,26 @@ def main():
     drawSineCurve(dart)
     drawCosineCurve(dart)
     drawTangentCurve(dart)
+    drawSecantCurve(dart)
+  
+    sides = int(input("\nHow many sides do you want your big shape to have? "))
+    length = int(input("\nChoose a side length greater than 200: "))
+    if length <= 200:
+      exit()
+    color = input("\nWhat color do you want your big shape to be? ")
+    perimeterB = drawBigShape(dart, wn, sides, length, color)
+    print("\nThe perimeter of your big shape is", perimeterB)
+  
+    sides = int(input("\nHow many sides do you want your little shape to have? "))
+    length = int(input("\nChoose a side length less than 150: "))
+    if length >= 150:
+      exit()
+    color = input("\nWhat color do you want your little shape to be? ")
+    perimeterL = drawLittleShape(dart, wn, sides, length, color)
+    print("\nThe perimeter of your little shape is", perimeterL)
+  
+    factor = perimeterB / perimeterL
+    print("\nThe perimeter of the big shape is", factor, "times greater than the perimeter of the little shape! ")
     wn.exitonclick()
 
 main()
